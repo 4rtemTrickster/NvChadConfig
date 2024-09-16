b6f42428c1a4edf379378ab4cbaeb3a186b0adca
4rtemTrickster <korneev.arty@yandex.ru>
2024-09-16 19:08:13 +0300

    Set up lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,14 +1,16 @@
--- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require("lspconfig")
 
--- EXAMPLE
-local servers = { "clangd" }
+lspconfig.servers = {
+    "lua_ls",
+}
+
+local default_servers = {}
+
 local nvlsp = require("nvchad.configs.lspconfig")
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
+for _, lsp in ipairs(default_servers) do
     lspconfig[lsp].setup({
         on_attach = nvlsp.on_attach,
         on_init = nvlsp.on_init,
@@ -16,9 +18,27 @@ for _, lsp in ipairs(servers) do
     })
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+lspconfig.lua_ls.setup({
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+
+    settings = {
+        Lua = {
+            diagnostics = {
+                enable = false,
+                -- globals = { "vim" },
+            },
+            workspace = {
+                library = {
+                    vim.fn.expand("$VIMRUNTIME/lua"),
+                    vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+                    vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+                    vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+            },
+        },
+    },
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,12 @@
 return {
     {
         "stevearc/conform.nvim",
-        -- event = 'BufWritePre', -- uncomment for format on save
         opts = require("configs.conform"),
     },
 
-    -- These are some examples, uncomment them if you want to see them work!
     {
         "neovim/nvim-lspconfig",
+        event = { "BufWritePre", "BufNewFile" },
         config = function()
             require("configs.lspconfig")
         end,
