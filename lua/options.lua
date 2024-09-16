4237220231a9becbb2d97d4ed9fe4157c27cffa6
4rtemTrickster <korneev.arty@yandex.ru>
2024-09-16 17:35:49 +0300

    Basic tweaks before rerformat

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,11 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.o
+local opt = vim.opt
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.relativenumber = true
+o.listchars = 'space:_,tab:>~'
+
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
