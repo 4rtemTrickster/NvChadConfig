512e93cbdef796ce7e4982ec5f8a30924e762d3b
4rtemTrickster <korneev.arty@yandex.ru>
2025-03-23 10:50:03 +0300

    Basic configuration

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,17 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+    theme = "chadracula",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    -- hl_override = {
+    -- 	Comment = { italic = true },
+    -- 	["@comment"] = { italic = true },
+    -- },
 }
 
 -- M.nvdash = { load_on_startup = true }
@@ -20,5 +20,35 @@ M.base46 = {
 --          lazyload = false
 --      }
 --}
+
+M.mason = {
+    command = true,
+    pkgs = {
+        -- lua
+        "lua-language-server",
+        "stylua",
+
+        -- c/cpp
+        "clangd",
+        "clang-format",
+
+        -- cmake
+        "cmakelint",
+        "cmakelang",
+        "neocmakelsp",
+
+        -- yaml
+        "yamlfmt",
+        "yamllint",
+        "yaml-language-server",
+
+        -- json
+        "json-lsp",
+        "jsonlint",
+
+        -- debugging
+        "codelldb",
+    },
+}
 
 return M
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,27 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.o
+local g = vim.g
+local opt = vim.opt
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+g.have_nerd_font = true
+
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+
+o.number = true
+o.relativenumber = true
+
+opt.cursorline = true
+
+opt.scrolloff = 10
+
+opt.list = true
+opt.listchars = {
+    eol = "⤶",
+    space = "·",
+    tab = "»-",
+}
+
+vim.opt.guicursor = ""
